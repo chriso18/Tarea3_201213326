9d569c28efa24cf4aa84f4e03e8d925ca7278f0e
chriso18 <christian.sierra.elias@gmail.com>
2014-08-19 17:06:38 -0500

    Programa si es Par o Impar

---
+++ numeros/src/numeros/par.java
@@ -0,0 +1,19 @@
+package numeros;
+
+import java.util.Scanner;
+
+public class par {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner S = new Scanner(System.in);
+int b;
+System.out.println("ingrese numero");
+ b = S.nextInt();
+  if ( b%2 == 0){
+	  System.out.println("par");
+  }else
+	  System.out.println("impar");	  
+	}
+	
+}
